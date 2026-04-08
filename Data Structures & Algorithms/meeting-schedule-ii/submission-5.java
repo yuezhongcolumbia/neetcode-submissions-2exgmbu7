c9be949a37f6c2503ef52ab72a54aa70ae55d685
yuezhongcolumbia <167635281+yuezhongcolumbia@users.noreply.github.com>
2026-04-07 22:40:42 -0400

    Add: meeting-schedule-ii - submission-5

---
+++ Data Structures & Algorithms/meeting-schedule-ii/submission-5.java
@@ -0,0 +1,31 @@
+/**
+ * Definition of Interval:
+ * public class Interval {
+ *     public int start, end;
+ *     public Interval(int start, int end) {
+ *         this.start = start;
+ *         this.end = end;
+ *     }
+ * }
+ */
+
+class Solution {
+    public int minMeetingRooms(List<Interval> intervals) {
+        // 1. it is asking us max number of interval at any given time;
+        // 2. we iterate interval by interval:
+        //     if start>= preEnd, no conflict, just update prevEnd
+        //     else conflict, count++
+        // 3. this doesnt solve the problem, because we forgot all previous interval end.
+        int res = 0;
+        PriorityQueue<Integer> minHeap = new PriorityQueue<>();
+        intervals.sort((a,b) -> a.start - b.start);
+        for (Interval interval: intervals){
+            while (!minHeap.isEmpty() && interval.start >= minHeap.peek()){
+                minHeap.poll();
+            }
+            minHeap.offer(interval.end);
+            res = Math.max(res,minHeap.size());
+        }
+        return res;
+    }
+}
