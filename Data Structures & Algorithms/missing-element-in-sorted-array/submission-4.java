12a58e3cbc412e95ece7e8b748ac0ef7d80ad13e
yuezhongcolumbia <167635281+yuezhongcolumbia@users.noreply.github.com>
2026-04-18 14:45:59 -0400

    Add: missing-element-in-sorted-array - submission-4

---
+++ Data Structures & Algorithms/missing-element-in-sorted-array/submission-4.java
@@ -0,0 +1,43 @@
+class Solution {
+    public int missingElement(int[] nums, int k) {
+    //    1.ascending order sorted
+    //    2. integer
+    //    3. unique
+
+    //    0,1,2,3, 4
+    //    4,6,9,12,13 k = 3 -> 10
+
+    //    locate the left nearest element.
+    //    so the number of missing integer at index i = nums[i] - nums[0] - i
+    //    we want to find the last idx that missing[i] < k
+
+    //     while(l < r){
+    //         missing = ....
+    //         if (missing < k){
+    //             l = m
+    //         }else {
+    //             r= m - 1
+    //         }
+    //     }
+
+    //     how much to increment?
+    //     k - missing
+        int l = 0, r = nums.length - 1;
+        int idx = -1;
+        while (l <= r){
+            int m = (l + r) >> 1;
+            int missing = nums[m] - nums[0] - m;
+            if (missing < k){
+                idx = m;
+                l = m + 1;
+            } else {
+                r = m - 1;
+            }
+        }
+        int gap = k - (nums[idx] - nums[0] - idx);
+        return nums[idx] + gap;
+
+      
+
+    }
+}
