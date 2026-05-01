a3710d6ab0a1b8d659b028e61d71b0e75a7bb24f
yuezhongcolumbia <167635281+yuezhongcolumbia@users.noreply.github.com>
2026-04-30 21:54:35 -0400

    Add: number-of-provinces - submission-0

---
+++ Data Structures & Algorithms/number-of-provinces/submission-0.java
@@ -0,0 +1,50 @@
+class DSU{
+    int[] parents;
+    public DSU(int n){
+        parents = new int[n];
+        for (int i = 0; i < n; i++){
+            parents[i] = i;
+        }
+    }
+    public int find(int x){
+        if (parents[x] == x) return x;
+        parents[x] = find(parents[x]);
+        return parents[x];
+    }
+    public boolean union(int x, int y){
+        int rx = find(x);
+        int ry = find(y);
+        if (rx == ry) return false;
+        if (rx <= ry){
+            parents[ry] = rx;
+        } else {
+            parents[rx] = ry;
+        }
+        return true;
+    }
+    public boolean isSame(int x, int y){
+        return find(x) == find(y);
+    }
+}
+class Solution {
+    public int findCircleNum(int[][] isConnected) {
+        // 1. dsu group them all up
+        // 2. n find, 
+        int n = isConnected.length;
+        DSU dsu = new DSU(n);
+        for (int i = 0; i < n; i++){
+            for (int j = 0; j < n; j++){
+                if (isConnected[i][j] == 1){
+                    dsu.union(i, j);
+                }
+            }
+        }
+        Set<Integer> set = new HashSet<>();
+        for (int i = 0; i< n; i++){
+            int root = dsu.find(i);
+            set.add(root);
+        }
+        return set.size();
+
+    }
+}
